4ba8dbcc0f7976cd1466fc21fcaaac43f142ae42
JoshCrozier <crozier4@gmail.com>
2015-04-12 22:37:50 -0400

    Adding the ability for a user to confirm the deletion of a Quirii

--- public/js/views/quiriiDetail.js
+++ public/js/views/quiriiDetail.js
@@ -5,12 +5,11 @@ define(['QuiriiNetView', 'text!templates/quiriiDetail.html', 'views/feedbackItem
     el: $('#content'),
 
     events: {
-      'click #deleteQuirii':'deleteQuirii'
+      'click #deleteQuirii': 'deleteQuirii'
     },
 
     initialize: function(options) {
       _.bindAll(this,'render');
-      var that = this;
       
       this.feedbackCollection = options.fbCollection;
       this.feedbackCollection.on('add', this.onFeedbackAdded, this);
@@ -22,23 +21,17 @@ define(['QuiriiNetView', 'text!templates/quiriiDetail.html', 'views/feedbackItem
       this.render();
     },
 
-    deleteQuirii: function(){
-      var that = this;
-      this.model.destroy({
-        wait: true,
-        success:function(model, response){
-          console.log("SHITS GONE", response, model);
-        },
-        error: function(model, response){
-          console.log("error ", response);
-        }
-      });
-      
-      return false;
+    deleteQuirii: function() {
+      var self = this,
+          confirmed = confirm('Are you sure you would like to delete this Quirii?');
+          
+      if (confirmed) {
+        this.model.destroy({ wait: true });
+      }
     },
 
-    quiriiDeleted: function(){
-      location.href='/';
+    quiriiDeleted: function() {
+      location.href = '/';
     },
 
     onFeedbackCollectionReset: function(collection) {
