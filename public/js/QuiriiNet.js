d3db7e38ad8c51a0c459aed8a25cf7786d63f739
JoshCrozier <crozier4@gmail.com>
2015-04-20 00:31:54 -0400

    More improvements to the 'back' button navigation functionality

--- public/js/QuiriiNet.js
+++ public/js/QuiriiNet.js
@@ -23,8 +23,27 @@ define(['router'], function(router) {
         }
 
         $('.prev', $nav).toggle(QuiriiScope.history.length > 1 && this.isAvailable(QuiriiScope.history[0]));
+      },
+      isAvailable: function (url) {
+        if (url !== undefined && (!(url === '' && Backbone.history.getFragment().indexOf('view') > -1))) {
+          return true;
+        }
+        return false;
+      },
+      previousPage: function () {
+        var url = QuiriiScope.history[QuiriiScope.history.length - 2],
+            previous = url ? url + '?prev=true' : 'view';
+
+        if (this.isAvailable(url)) {
+          Backbone.history.navigate('#/' + previous, { replace: true });
+          QuiriiScope.history = QuiriiScope.history.slice(0, -1);
+        }
       }
     }
+    
+    $(document).on('click.CUSTOMLALLSDLASDLLDSA', '#navigation .prev', function (e) {
+      QuiriiScope.previousPage();
+    });
       
     window.QuiriiScope = scope;
     Backbone.history.start();
