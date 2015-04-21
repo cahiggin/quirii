926be0f80131952ba29e88c5d3cbdc7513de0d4d
JoshCrozier <crozier4@gmail.com>
2015-04-20 22:09:02 -0400

    More updates around the Tiwtter share button

--- public/js/views/giveQuiriiFeedback.js
+++ public/js/views/giveQuiriiFeedback.js
@@ -70,18 +70,22 @@ function(QuiriiNetView, morphyUiTemplate, Morphii,
 
         self.model.save(null, {
           success: function () {
-            var container = $('#public-quirii'),
-                shareAnchor = $('<a>').attr('href', QuiriiScope.twitterShareLink('Quirii:')).addClass('big-btn blue').html('<i class="fa fa-twitter"></i> Tweet this Quirii');
-                
-            $('.col-sm-8:not(.hidden)', container).remove();
-            $('.col-sm-8.hidden', container).removeClass('hidden');
-            $('.col-sm-8', container).append(shareAnchor);
+            self.renderFeedbackSuccessView();
           }
         });
       } else {
         this.$el.addClass('has-error');
         this.$el.find('.control-label').removeClass('hidden');
       } 
+    },
+    
+    renderFeedbackSuccessView: function () {
+      var container = $('#public-quirii'),
+          shareAnchor = $('<a>').attr('href', QuiriiScope.twitterShareLink('Quirii:')).addClass('big-btn twitter').html('<i class="fa fa-twitter"></i> Tweet this Quirii');
+          
+     $('.col-sm-8:not(.hidden)', container).remove();
+     $('.col-sm-8.hidden', container).removeClass('hidden');
+     $('.col-sm-8', container).append(shareAnchor);
     },
 
     renderMorphii: function(morphiiType){
