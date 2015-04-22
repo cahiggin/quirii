4e090af4bfaa97cc8f0fae0efbceae2d0be99255
JoshCrozier <crozier4@gmail.com>
2015-04-22 03:50:15 -0400

    Adding a link to allow the user to go back and view previous quiriis if the back button isn't present

--- public/js/QuiriiNet.js
+++ public/js/QuiriiNet.js
@@ -25,6 +25,7 @@ define(['router'], function(router) {
 
         $('.prev', $nav).toggle(QuiriiScope.history.length > 1 && fragment.indexOf('create') !== 0 && fragment.indexOf('view') !== 0);
         $('.create', $nav).toggle(fragment.indexOf('view') !== -1 || fragment === '');
+        $('#view-all-quiriis').toggle(fragment.indexOf('me/quiriis') !== -1);
       },
       previousPage: function () {
         var url = QuiriiScope.history[QuiriiScope.history.length - 2],
@@ -43,7 +44,7 @@ define(['router'], function(router) {
     $(document).on('click.QuiriiScope', '#navigation .prev', function (e) {
       QuiriiScope.previousPage();
     });
-    $(document).on('click.QuiriiScope', '#navigation .cancel', function (e) {
+    $(document).on('click.QuiriiScope', '#navigation .cancel, #view-all-quiriis', function (e) {
       Backbone.history.navigate('#/view', { replace: true });
     });
     $(document).on('click.QuiriiScope', '#navigation .create, #add-new-quirii', function (e) {
