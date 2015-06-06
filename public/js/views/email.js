fa0e3b643322512945b9d994544e75c1519dfec3
JoshCrozier <crozier4@gmail.com>
2015-06-06 00:08:07 -0400

    Adding the email workflow

---
+++ public/js/views/email.js
@@ -0,0 +1,51 @@
+define(['QuiriiNetView', 'text!templates/email.html'],
+function(QuiriiNetView, emailTemplate) {
+  var emailView = QuiriiNetView.extend({
+    el: $('#content'),
+
+    events: {
+      'click #submit-email': 'handleEmail'
+    },
+    
+    handleEmail: function () {
+      this.updateEmail(this.$el.find('#email').val());
+    },
+    
+    updateEmail: function (email) {
+      var self = this;
+      
+      this.user.email = email;
+      $.ajax({
+        method: 'PUT',
+        url: '/api/me/settings',
+        data: self.user,
+        success: function (data) {
+          console.log(data);
+        }
+      });
+    },
+    
+    initialize: function() {
+      var self = this;
+      
+      $.ajax({
+        method: 'GET',
+        url: '/api/me/settings',
+        success: function (data) {
+          self.user = data.data.user;
+          console.log(self.user);
+          
+          if (self.user.email) {
+          } else {
+          }
+        }
+      });
+    },
+
+    render: function() {
+      this.$el.html(emailTemplate);
+    }
+  });
+
+  return emailView;
+});
